2c4a2bf05aff852a37e48b3790b727b6c0d050b7
Erencan Pelin <88778734+ErencanPelin@users.noreply.github.com>
2023-07-15 14:08:05 +1000

    Feature show similar articles (#2)
    * Update popup.js
    
    * Clean up code
    
    * Fix broken import links

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,40 @@
+import { getNews } from "./../scripts/newsAPI.js";
+
 var tab_title = '';
+var articleHeading = "";
 
-/* document.getElementById('summarizeBtn').addEventListener("click", function (event) {
-
+//generate summary button click event
+document.getElementById('summarizeBtn').addEventListener('click', function (event) {
+  console.log("generating summary");
+  document.getElementById("summaryOut").textContent = "Generating Summary. Please wait...";
+  document.getElementById('summarizeBtn').remove();
+  //send query to newsAPI
+  let q = articleHeading;
+  getNews();
 });
- */
-const functionToExecute = () => {
+
+//read heading from page
+const readH1 = () => {
   return document.querySelector("h1").textContent;
 };
 
+//executed when extension is opened
 chrome.tabs.query({ active: true }, function (tabs) {
   var tab = tabs[0];
   tab_title = tab.title;
-
   chrome.scripting.executeScript(
-      {
-          target: { tabId: tab.id },
-          //files: ["/scripts/content.js"],
-          function: functionToExecute
-      },
-       (res) => {
-          var out = res[0].result;
-          if (out == null) out = "Nothing relevant found.";
-          document.getElementById("output").textContent = '"' + out + '"';
-      })
+    {
+      target: { tabId: tab.id },
+      function: readH1
+    },
+    (res) => {
+      var out = res[0].result;
+      articleHeading = out;
+      if (out == null) {
+        articleHeading = null;
+        document.getElementById("articleHeadingOut").textContent = "No other relevant news articles found.";
+        return;
+      }
+      document.getElementById("articleHeadingOut").textContent = '"' + out + '"';
+    })
 });
