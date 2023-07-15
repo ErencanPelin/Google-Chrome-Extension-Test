358a74554018b20e1d1310d65c2a566aee813760
Erencan Pelin <erencan.pelin@gmail.com>
2023-07-15 10:15:27 +1000

    clean up

--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,26 @@
 var tab_title = '';
-function display_h1(results) {
-    h1 = results;
-    document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
-}
 
+/* document.getElementById('summarizeBtn').addEventListener("click", function (event) {
+
+});
+ */
 const functionToExecute = () => {
-    return document.querySelector("h1").textContent;
+  return document.querySelector("h1").textContent;
 };
 
 chrome.tabs.query({ active: true }, function (tabs) {
-    var tab = tabs[0];
-    tab_title = tab.title;
+  var tab = tabs[0];
+  tab_title = tab.title;
 
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tab.id },
-            function: functionToExecute
-        },
-        (res) => { 
-            var out = res[0].result;
-            if (out == null) out = "Nothing relevant found.";
-            document.getElementById("output").textContent = out; 
-        })
-
-    /*document.querySelector("h1").textContent*/
+  chrome.scripting.executeScript(
+      {
+          target: { tabId: tab.id },
+          //files: ["/scripts/content.js"],
+          function: functionToExecute
+      },
+       (res) => {
+          var out = res[0].result;
+          if (out == null) out = "Nothing relevant found.";
+          document.getElementById("output").textContent = '"' + out + '"';
+      })
 });
