f62aefd6a5a462b3a9baf25b8852ee799f471c1f
Erencan Pelin <88778734+ErencanPelin@users.noreply.github.com>
2023-07-16 09:47:44 +1000

    Feature update gui (#4)
    * Update popup.html
    
    * Feature show similar articles (#2) (#3)
    
    * Update popup.js
    
    * Clean up code
    
    * Fix broken import links
    
    * make gui pretty
    
    * find keywords
    
    * fix btn padding
    
    * fix keyword finding

--- popup/popup.js
+++ popup/popup.js
@@ -13,9 +13,45 @@ document.getElementById('summarizeBtn').addEventListener('click', function (even
   getNews();
 });
 
+//find articles in those keywords
+var getArticles = async (keywords) => {
+  var q = keywords.replace(' ', '-');
+  q = "https://themilsource.com/tag/" + q;
+  console.log(q);
+
+  //request
+  var response = await fetch(q);
+    switch (response.status) {
+        // status "OK"
+        case 200:
+            var template = await response.body.getReader().read();
+
+            console.log(template);
+            break;
+        // status "Not Found"
+        case 404:
+            console.log('Not Found');
+            break;
+    }
+}
+
+//find keywords in heading
+function getKeywords(head) {
+  var keywords = "";
+  const regex = /(([A-Z]|[0-9])(([A-Z]|[a-z]|[0-9])*))/g;
+  const found = head.toString().match(regex);
+  found.forEach(element => {
+    keywords += element + ", ";
+  });
+
+  document.getElementById("articleKeywords").textContent = keywords;//.replace(" ", "', '");
+  return keywords;
+}
+
 //read heading from page
 const readH1 = () => {
-  return document.querySelector("h1").textContent;
+  var articleHead = document.querySelector("h1").textContent;
+  return articleHead;
 };
 
 //executed when extension is opened
@@ -32,9 +68,11 @@ chrome.tabs.query({ active: true }, function (tabs) {
       articleHeading = out;
       if (out == null) {
         articleHeading = null;
-        document.getElementById("articleHeadingOut").textContent = "No other relevant news articles found.";
+        document.getElementById("articleHeadingOut").textContent = "No article found.";
         return;
       }
+      let keywords = getKeywords(out);
+      getArticles(keywords);
       document.getElementById("articleHeadingOut").textContent = '"' + out + '"';
     })
 });
