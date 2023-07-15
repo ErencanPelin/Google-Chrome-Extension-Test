9a2acbefb265a4380c42d441575da2c7e9bb6516
Erencan Pelin <erencan.pelin@gmail.com>
2023-07-15 12:32:04 +1000

    Clean up code

--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,40 @@
+import { getNews } from "../scripts/newsAPI";
+
 var tab_title = '';
 var articleHeading = "";
 
-const functionToExecute = () => {
+//generate summary button click event
+document.getElementById('summarizeBtn').addEventListener('click', function (event) {
+  console.log("generating summary");
+  document.getElementById("summaryOut").textContent = "Generating Summary. Please wait...";
+  document.getElementById('summarizeBtn').remove();
+  //send query to newsAPI
+  let q = articleHeading;
+  getNews();
+});
+
+//read heading from page
+const readH1 = () => {
   return document.querySelector("h1").textContent;
 };
 
+//executed when extension is opened
 chrome.tabs.query({ active: true }, function (tabs) {
   var tab = tabs[0];
   tab_title = tab.title;
-
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id },
-      //files: ["/scripts/content.js"],
-      function: functionToExecute
+      function: readH1
     },
     (res) => {
       var out = res[0].result;
       articleHeading = out;
       if (out == null) {
         articleHeading = null;
-        document.getElementById("output").textContent = "No other relevant news articles found.";
+        document.getElementById("articleHeadingOut").textContent = "No other relevant news articles found.";
         return;
       }
-      document.getElementById("output").textContent = '"' + out + '"';
+      document.getElementById("articleHeadingOut").textContent = '"' + out + '"';
     })
 });
---
+++ scripts/newsAPI.js
@@ -0,0 +1,18 @@
+import { newsAPI_Key } from "../secrets.js";
+
+var apiKey = newsAPI_Key;
+
+let requestURL = 'https://newsapi.org/v2/everything?apiKey=' + apiKey;
+var getNews = async () => {
+    let response = await fetch(requestURL);
+    console.log(response);
+    if (!response.ok) {
+        alert("Data unavailable at the moment. Please try again later");
+        return null;
+    }
+    let data = await response.json();
+    console.log(data);
+    return data.articles;
+};
+
+export { getNews };
